b757f5c1bf4889e082e10ffc404fcdb0c65820b7
vitalii.vlo <vitalii.vlo@gmail.com>
2020-06-25 10:30:42 +0300

    Move sensitive data to env file

--- safe_internet/settings.py
+++ safe_internet/settings.py
@@ -160,5 +160,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'safeinternetemailtesting444@gmail.com'
-EMAIL_HOST_PASSWORD = 'testing444'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
